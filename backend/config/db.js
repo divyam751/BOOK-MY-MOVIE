5213e3b9eb8ac5bbcfe2f125dda6ffad4da19f60
Divyam Chauhan <125983433+divyam751@users.noreply.github.com>
2024-02-19 12:19:17 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 // const connection = mongoose.connect(process.env.MONGO_URL);
 const connection = mongoose.connect(
-  "mongodb+srv://Sample:bfTKg6CAam014lTJ@cluster0.e8nwm9g.mongodb.net/ticket-flix"
+  // url removed
 );
 
 module.exports = { connection };
